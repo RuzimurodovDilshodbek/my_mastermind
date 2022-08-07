3d0288afdc05f94fcfecea85d155985f5560c394
javohir <javohir@gmail.com>
2022-08-07 16:43:35 +0500

    fix

---
+++ database/seeders/UserAdminSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::updateOrCreate(
+            [
+                'name' => 'Admin',
+                'email' => 'Admin@gmail.com',
+                'password' => 'admin12345',
+                'is_admin' => true,
+            ]
+        );
+        //
+    }
+}
